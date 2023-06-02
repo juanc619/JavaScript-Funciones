2b5fcb73312045abc9f86f17f310f0bd5bb92339
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-02 17:01:12 -0500

    funciones declaradas con retorno y flecha con retorno implicito

--- main.js
+++ main.js
@@ -15,7 +15,22 @@
 
 // Funciones flecha
 
-const saludar = (nombre) => {
-    console.log ("Hola " + nombre);
+// const saludar = (nombre) => {
+//     console.log ("Hola " + nombre);
+// }
+// saludar("Jc");
+
+// Un solo parametro y una sola sentencia : se puedes obviar los parentesis y las llaves
+
+// const saludar = nombre => console.log("Hola " + nombre);
+
+//Funciones declaradas con retorno
+function suma (a, b) {
+    return a + b;
 }
-saludar("Jc");
+
+// Funciones flecha con retorno implicito
+const resta = (a, b) => a-b;
+
+console.log(suma(5, 5));
+console.log(resta(10, 3));
