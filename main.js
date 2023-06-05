e75f9f391eaf656acc4da96d6eca374ae105b68c
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-05 18:07:26 -0500

    Objeto con funcion constructora

--- main.js
+++ main.js
@@ -25,12 +25,56 @@
 // const saludar = nombre => console.log("Hola " + nombre);
 
 //Funciones declaradas con retorno
-function suma (a, b) {
-    return a + b;
+// function suma (a, b) {
+//     return a + b;
+// }
+
+// // Funciones flecha con retorno implicito
+// const resta = (a, b) => a-b;
+
+// console.log(suma(5, 5));
+// console.log(resta(10, 3));
+
+// function suma (a, b) {
+//     return a + b;
+// }
+// console.log(suma(5, 15));
+
+// function suma (a, b) {
+//     return a - b;
+// }
+// console.log(suma(20,10));
+
+// const usuario1 = {
+//     nombre : "Juan carlos",
+//     edad :  33,
+//     nacionalidad : "peruana"
+// }
+
+// console.log(usuario1.nacionalidad);
+
+// const usuario3 = {
+//     name : "jc",
+//     year : 33,
+//     nation : "peruvian",
+//         hobbies : {
+//             hobbie1 : "futbol",
+//             hobbie2 : "leer",
+//             hobbie3 : "escribir"
+//         }
+// }
+
+// console.log(usuario3.hobbies.hobbie2);
+
+// Contructor
+
+function Producto(titulo, color, precio) {
+    this.titulo = titulo;
+    this.color = color;
+    this.precio = precio;
 }
 
-// Funciones flecha con retorno implicito
-const resta = (a, b) => a-b;
+const product1 = new Producto("silla", "rojo", 50);
+const product2 = new Producto("cama", "blanca" , 1500);
 
-console.log(suma(5, 5));
-console.log(resta(10, 3));
+console.log(product2);
